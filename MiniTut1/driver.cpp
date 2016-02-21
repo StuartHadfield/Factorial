be31fb26bac26b82fd62a1ab88f2e345ba972fe0
Stuart <stuartdhadfield@gmail.com>
2016-02-21 19:18:39 +0200

    added user input run command

--- MiniTut1/driver.cpp
+++ MiniTut1/driver.cpp
@@ -6,8 +6,18 @@
 
 #include <iostream>
 #include "factorial.h"
+#include <string>
 int main(int argc, char** argv) {
 	using namespace std; //never use "using namespace" outside a scope... very bad practice, especially if you do it out of scope in a header file
+	
+	string mystring = "";
+	
+	while (mystring.compare("make run") != 0)
+	{
+		cout << "Please input the run command to execute the factorial program" << endl;
+		getline(cin,mystring);
+	}
+	
 	cout << "The factorial of 0 is: " << factorial(0) << endl;
 	cout << "The factorial of 1 is: " << factorial(1) << endl;
 	cout << "The factorial of 5 is: " << factorial(5) << endl;
